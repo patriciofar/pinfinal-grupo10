4132c0cb2c770055fb32385cdb292f4b7055d353
edison.farinango <edison.farinango@megaprofer.com>
2024-11-27 12:42:25 -0500

    deploy configuration

---
+++ 01_eksctl/create-cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set AWS credentials 
+aws sts get-caller-identity >> /dev/null
+if [ $? -eq 0 ]
+then
+  echo "Credenciales testeadas, proceder con la creacion de cluster."
+
+  # Creacion de cluster
+  eksctl create cluster \
+  --name eks-mundos-e \
+  --region us-east-1 \
+  --nodes 3 \
+  --node-type t3.small \
+  --with-oidc \
+  --ssh-access \
+  --ssh-public-key pin-grupo10 \
+  --managed \
+  --full-ecr-access \
+  --zones us-east-2a,us-east-2b,us-east-2c
+
+  if [ $? -eq 0 ]
+  then
+    echo "Cluster Setup Completo con eksctl ."
+  else
+    echo "Cluster Setup Falló mientras se ejecuto eksctl."
+  fi
+else
+  echo "Please run aws configure & set right credentials."
+  echo "Cluster setup failed."
+fi
