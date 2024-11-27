af82e92baea7e3224ff152dadb2ba9b2e31ba044
edison.farinango <edison.farinango@megaprofer.com>
2024-11-27 14:34:07 -0500

    fix

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -14,8 +14,7 @@ mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --client
 
-echo "Installing ekctl"
-# Download EKS CLI https://github.com/weaveworks/eksctl
+echo "Installing eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 export PATH=$PATH:/usr/local/bin
@@ -41,4 +40,4 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum install -y terraform
 
-
+echo "Instalación completada"
