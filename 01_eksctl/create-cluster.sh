2737e3e69e50aa9e7462d4c7ad0a5407f9afc474
edison.farinango <edison.farinango@megaprofer.com>
2024-11-29 12:46:42 -0500

    Fix create instance ec2

--- 01_eksctl/create-cluster.sh
+++ 01_eksctl/create-cluster.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# Set AWS credentials 
+# Comprobando si AWS CLI está configurado correctamente
 aws sts get-caller-identity >> /dev/null
-if [ $? -eq 0 ]
-then
-  echo "Credenciales testeadas, proceder con la creacion de cluster."
+if [ $? -eq 0 ]; then
+  echo "Credenciales AWS configuradas correctamente. Procediendo con la creación del cluster."
 
-  # Creacion de cluster
+  # Verificando si eksctl está instalado
+  command -v eksctl >/dev/null 2>&1 || { echo >&2 "eksctl no está instalado. Instalando..."; exit 1; }
+
+  # Creación del cluster en EKS
   eksctl create cluster \
-  --name eks-mundos-e \
-  --region us-east-1 \
-  --nodes 3 \
-  --node-type t3.small \
-  --with-oidc \
-  --ssh-access \
-  --ssh-public-key pin-grupo10 \
-  --managed \
-  --full-ecr-access \
-  --zones us-east-1a,us-east-1b,us-east-1c
+    --name eks-mundos-e \
+    --region us-east-1 \
+    --nodes 3 \
+    --node-type t3.small \
+    --with-oidc \
+    --ssh-access \
+    --ssh-public-key pin-grupo10 \
+    --managed \
+    --full-ecr-access \
+    --zones us-east-1a,us-east-1b,us-east-1c
 
-  if [ $? -eq 0 ]
-  then
-    echo "Cluster Setup Completo con eksctl ."
+  if [ $? -eq 0 ]; then
+    echo "Cluster creado exitosamente con eksctl."
   else
-    echo "Cluster Setup Falló mientras se ejecuto eksctl."
+    echo "Error: La creación del cluster falló al ejecutar eksctl."
   fi
 else
-  echo "Please run aws configure & set right credentials."
-  echo "Cluster setup failed."
+  echo "No se encuentran credenciales de AWS configuradas. Por favor, ejecuta 'aws configure' para configurar las credenciales adecuadas."
+  echo "El setup del cluster ha fallado."
+  exit 1
 fi
