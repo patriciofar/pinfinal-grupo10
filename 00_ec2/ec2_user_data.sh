1b8ae7b32346ff9f6ea095e410c7b3e55a47d3b1
edison.farinango <edison.farinango@megaprofer.com>
2024-11-29 12:35:17 -0500

    Fix create instance ec2

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Instalación de AWS CLI en Ubuntu
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -7,12 +8,13 @@ sudo ./aws/install --update
 aws --version
 echo "AWS CLI installed successfully"
 
+# Instalación de kubectl
 echo "Installing kubectl"
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
+echo "kubectl installed successfully"
 
 echo "Installing eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
