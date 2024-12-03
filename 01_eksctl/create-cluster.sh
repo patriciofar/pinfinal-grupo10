109eb71e6d3ab4563f4ab14d9c78c0ff5c4790e1
edison.farinango <edison.farinango@megaprofer.com>
2024-12-03 12:15:58 -0500

    Fix version of eks

--- 01_eksctl/create-cluster.sh
+++ 01_eksctl/create-cluster.sh
@@ -61,7 +61,7 @@ if [ $? -eq 0 ]; then
     --managed \
     --full-ecr-access \
     --zones us-east-1a,us-east-1b,us-east-1c \
-  --version 1.23
+  --version 1.22
 
   if [ $? -eq 0 ]; then
     echo "Cluster creado exitosamente con eksctl."
