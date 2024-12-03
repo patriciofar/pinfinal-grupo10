c386daf93edc4074468a763d32d4ba2468102d90
edison.farinango <edison.farinango@megaprofer.com>
2024-12-03 15:03:56 -0500

    Fix implement eks

--- 01_eksctl/create-cluster.sh
+++ 01_eksctl/create-cluster.sh
@@ -1,53 +1,14 @@
 #!/bin/bash
 
-# Comprobando si AWS CLI está configurado correctamente
+# Variables
+CLUSTER_NAME=mundoes-cluster-G6
+AWS_REGION=us-east-2
+
+# Set AWS credentials 
 aws sts get-caller-identity >> /dev/null
-if [ $? -eq 0 ]; then
-  echo "Credenciales AWS configuradas correctamente. Procediendo con la creación del cluster."
-
-  # Verificando si eksctl está instalado
-  if ! command -v eksctl &> /dev/null; then
-    echo "eksctl no está instalado. Instalando..."
-
-    # Descargando e instalando eksctl
-    curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/v0.114.0/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
-    if [ $? -eq 0 ]; then
-      sudo mv /tmp/eksctl /usr/local/bin/eksctl
-      sudo chmod +x /usr/local/bin/eksctl
-      echo "eksctl instalado correctamente."
-    else
-      echo "Error al instalar eksctl."
-      exit 1
-    fi
-  else
-    echo "eksctl ya está instalado."
-  fi
-
-  # Verificando si el clúster existe
-  CLUSTER_NAME="eks-mundos-e"
-  CLUSTER_EXISTS=$(eksctl get clusters --region us-east-1 --name "$CLUSTER_NAME" --output json)
-
-  # Si el clúster existe, eliminamos el clúster antes de crear uno nuevo
-  if [ ! -z "$CLUSTER_EXISTS" ]; then
-    echo "El clúster '$CLUSTER_NAME' ya existe. Eliminándolo primero..."
-    
-    # Eliminando la pila de CloudFormation asociada al clúster si existe
-    aws cloudformation delete-stack --stack-name "eksctl-eks-mundos-e-cluster" --region us-east-1
-
-    # Esperar hasta que la pila sea eliminada
-    echo "Esperando a que la pila de CloudFormation se elimine..."
-    aws cloudformation wait stack-delete-complete --stack-name "eksctl-eks-mundos-e-cluster" --region us-east-1
-
-    # Verificando que la pila se haya eliminado correctamente
-    if [ $? -eq 0 ]; then
-      echo "El clúster '$CLUSTER_NAME' ha sido eliminado correctamente."
-    else
-      echo "Error al eliminar el clúster '$CLUSTER_NAME'."
-      exit 1
-    fi
-  else
-    echo "No se encontró el clúster '$CLUSTER_NAME'. Procediendo con la creación."
-  fi
+if [ $? -eq 0 ]
+then
+  echo "Credenciales testeadas, proceder con la creacion de cluster."
 
   # Creación del cluster en EKS
   eksctl create cluster \
